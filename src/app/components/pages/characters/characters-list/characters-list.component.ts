058c417c3ea1fdf26e0b2bfe930c92c985104b63
Tiago Yamamoto <japa@rede5.com.br>
2021-05-25 15:52:36 -0300

    Recuperamos la data desde GraphQL y la guardamos en observables

--- src/app/components/pages/characters/characters-list/characters-list.component.ts
+++ src/app/components/pages/characters/characters-list/characters-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { DataService } from '@app/shared/services/data.service';
 
 @Component({
   selector: 'app-characters-list',
@@ -6,8 +7,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./characters-list.component.scss']
 })
 export class CharactersListComponent implements OnInit {
+  characters$ = this.dataSvc.characters$;
 
-  constructor() { }
+  constructor(private dataSvc: DataService) { }
 
   ngOnInit(): void {
   }
--- src/app/shared/services/data.service.ts
+++ src/app/shared/services/data.service.ts
@@ -1,5 +1,7 @@
 import { Injectable } from '@angular/core';
 import { Apollo, gql } from 'apollo-angular';
+import { BehaviorSubject, of } from 'rxjs';
+import { catchError, find, mergeMap, pluck, take, tap, withLatestFrom } from 'rxjs/operators';
 
 const QUERY = gql`
 {
@@ -27,5 +29,26 @@ const QUERY = gql`
 })
 export class DataService {
 
-  constructor() { }
+  private episodesSubject = new BehaviorSubject<any[]>([]);
+  episodes$ = this.episodesSubject.asObservable();
+
+  private charactersSubject = new BehaviorSubject<any[]>([]);
+  characters$ = this.charactersSubject.asObservable();
+
+  constructor(private apollo: Apollo) { 
+    this.getDataApi();
+  }
+
+  private getDataApi(): void {
+    this.apollo.watchQuery<any>({
+      query: QUERY
+    }).valueChanges.pipe(
+      take(1),
+      tap(({data}) => {
+        const { characters, episodes } = data;
+        this.episodesSubject.next(episodes.results);
+        this.charactersSubject.next(characters.results);
+      })
+    ).subscribe();
+  }
 }
