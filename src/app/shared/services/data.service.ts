04216d9df3a3e8a2b17b30df3095b4df24548c21
Tiago Yamamoto <japa@rede5.com.br>
2021-05-25 14:57:11 -0300

    Generamos data service para recuperar la data desde GraphQL

---
+++ src/app/shared/services/data.service.ts
@@ -0,0 +1,10 @@
+import { Injectable } from '@angular/core';
+import { Apollo, gql } from 'apollo-angular';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DataService {
+
+  constructor() { }
+}
