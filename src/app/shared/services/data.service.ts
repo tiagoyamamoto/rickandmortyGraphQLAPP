81f755d8616e884384abb8203eb1d54ab4ac74b0
Tiago Yamamoto <japa@rede5.com.br>
2021-05-25 15:06:21 -0300

    Graphql Playground - ricky and morty API

--- src/app/shared/services/data.service.ts
+++ src/app/shared/services/data.service.ts
@@ -1,6 +1,27 @@
 import { Injectable } from '@angular/core';
 import { Apollo, gql } from 'apollo-angular';
 
+const QUERY = gql`
+{
+ episodes {
+   results {
+     name
+     episode
+   }
+ }
+  characters {
+   results {
+     id
+     name
+     status
+     species
+     gender
+     image
+   }
+ }
+}`;
+
+
 @Injectable({
   providedIn: 'root'
 })
